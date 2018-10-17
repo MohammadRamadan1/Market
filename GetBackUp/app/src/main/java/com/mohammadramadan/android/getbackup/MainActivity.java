0827af590fa501684499033b3b6ddcd2136d430f
MohammadRamadan2 <35143572+MohammadRamadan2@users.noreply.github.com>
2018-10-17 19:45:53 +0200

    Market

---
+++ GetBackUp/app/src/main/java/com/mohammadramadan/android/getbackup/MainActivity.java
@@ -0,0 +1,92 @@
+package com.mohammadramadan.android.getbackup;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    /** Vor Sie den Code lessen, id "mouse" in activity_main.xml gehört zu dem {@keyboard checkBox}
+     *  und id "keyboard" in activity_main.xml gehört zu dem  {@mouse checkbox}
+     **/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    int quantity = 2;
+
+    public void minus(View view) {
+        quantity = quantity - 1;
+        displayQuanity(quantity);
+        if ( quantity<= 0){
+            Toast.makeText(getApplicationContext(),"You Can't buy less than 1 ", Toast.LENGTH_LONG).show();
+        }
+        else{
+            return;
+        }
+    }
+
+    public void plus(View view) {
+        quantity = quantity + 1;
+        displayQuanity(quantity);
+        if (quantity >= 21){
+            Toast.makeText(getApplicationContext(),"You Can't buy more than 20 ", Toast.LENGTH_LONG).show();
+        }
+        else{
+            return;
+        }
+    }
+
+    public void displayQuanity(int numberOfStuff) {
+        TextView quanityTextView = (TextView) findViewById(R.id.number_of_stuff);
+        quanityTextView.setText("" + numberOfStuff);
+    }
+
+    public void buyMessage (View view) {
+        CheckBox mouseCheckBox = (CheckBox) findViewById(R.id.mouse);
+        boolean hasMouseChexkBox = mouseCheckBox.isChecked();
+
+        CheckBox keyboardCheckBox = (CheckBox) findViewById(R.id.keyboard);
+        boolean hasKeyboardCheckBox= keyboardCheckBox.isChecked();
+
+        int price = calculaterPrice();
+
+        String message =createOrderSummery(price, hasKeyboardCheckBox, hasMouseChexkBox);
+        displayMessage(message);
+    }
+
+
+    public int calculaterPrice() {
+        return quantity * 100;
+    }
+
+    private String createOrderSummery (int price, boolean addMouseCheckBox, boolean addKeyboardCheckBox){
+        String priceMessage = "Thanks for using the App!";
+
+        priceMessage +="\nadd keyboard? " + addKeyboardCheckBox;
+        if (addKeyboardCheckBox) {
+            price = price + 10;
+        }
+        priceMessage +="\nadd mouse? " + addMouseCheckBox;
+        if (addMouseCheckBox){
+            price = price +5;
+        }
+
+        priceMessage +="\n total: "+ price +"€";
+        return  priceMessage;
+    }
+
+    private void displayMessage(String message){
+        TextView orderSummeryTextView = (TextView) findViewById(R.id.order);
+        orderSummeryTextView.setText(message);
+    }
+}
